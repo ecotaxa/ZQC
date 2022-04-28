e2d1b248fa21c7a49152f20b1805c4cca3d2f6a0
juliecoust <julie.coustenoble@imev-mer.fr>
2022-04-28 14:58:51 +0200

    CI simple test

--- dash_test/test_CI.py
+++ dash_test/test_CI.py
@@ -1,9 +1,13 @@
-import pytest
+from dash.testing.application_runners import import_app
 
-def test_title(self):
-    self.driver.get('https://www.delrayo.tech')
-    assert self.driver.title == "DelRayo.tech - Delrayo Tech"
+def test_title(dash_duo):
+    app = import_app("dash_test.app")
+    dash_duo.start_server(app)
+    dash_duo.driver.get('https://www.delrayo.tech')
+    assert dash_duo.driver.title == "DelRayo.tech - Delrayo Tech"
 
-def test_title_blog(self):
-    self.driver.get('https://www.delrayo.tech/blog')
-    print(self.driver.title)
+def test_title_blog(dash_duo):
+    app = import_app("dash_test.app")
+    dash_duo.start_server(app)
+    dash_duo.driver.get('https://www.delrayo.tech/blog')
+    print(dash_duo.driver.title)
