1a0a6329b316813a7c48818be9d844534bf63666
juliecoust <julie.coustenoble@imev-mer.fr>
2022-04-28 11:34:12 +0200

    CI main.yml

--- componants.py
+++ componants.py
@@ -17,7 +17,7 @@ def generate_project_selector(drives):
         html.H2("Drive"),
         dcc.Dropdown(
             id='app-1-dropdown-drives',
-            value="zooscan_lov",
+            value="",
             clearable = False,
             options=[
                 {'label': drive['label'] + " 🔒" if drive['disabled'] else drive['label'], 
