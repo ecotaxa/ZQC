867661028385df9fd98e5d2201616e46fcb54784
juliecoust <julie.coustenoble@imev-mer.fr>
2022-04-28 16:14:28 +0200

    CI test integration

--- dash_test/test_integration.py
+++ dash_test/test_integration.py
@@ -23,6 +23,10 @@ def test_multi_page_app_is_running(dash_duo) :
     e = dash_duo.driver.find_element(by=By.CSS_SELECTOR, value=".help-btn")
     e.click()
     assert dash_duo.driver.find_element(by=By.CSS_SELECTOR, value="h1").text == "Presentation of Data quality checker tool for zooscan projects"
+   
+    # Go from doc to app
+    dash_duo.wait_for_page(dash_duo.server_url + '/QC/zooscan/doc', timeout=5)
     e_go_qc = dash_duo.driver.find_element(by=By.CSS_SELECTOR, value=".go-qc")
     e_go_qc.click()
     assert dash_duo.driver.find_element(by=By.CSS_SELECTOR, value="h1").text == "Data quality checks"
+    dash_duo.wait_for_page(dash_duo.server_url + '/QC/zooscan/doc', timeout=5)
