fd8d5927d21287378eb2b75add46589441123222
juliecoust <julie.coustenoble@imev-mer.fr>
2022-04-28 16:29:50 +0200

    CI test integration

--- dash_test/test_integration.py
+++ dash_test/test_integration.py
@@ -16,17 +16,14 @@ def test_multi_page_app_is_running(dash_duo) :
     dash_duo.wait_for_page(dash_duo.server_url + '/QC/zooscan/doc', timeout=5)
     assert dash_duo.driver.find_element(by=By.CSS_SELECTOR, value="h1").text == "Presentation of Data quality checker tool for zooscan projects"
     assert dash_duo.get_logs() == [], "Browser console contain no error"
-    
     #Go to all the pages of the dash app and check llinks between pages
+    # Go from doc to app
+    e_go_qc = dash_duo.driver.find_element(by=By.CSS_SELECTOR, value=".go-qc")
+    e_go_qc.click()
+    assert dash_duo.driver.find_element(by=By.CSS_SELECTOR, value="h1").text == "Data quality checks"
+
     # Go from app to doc
     dash_duo.wait_for_page(dash_duo.server_url + '/QC/zooscan', timeout=5)
     e = dash_duo.driver.find_element(by=By.CSS_SELECTOR, value=".help-btn")
     e.click()
     assert dash_duo.driver.find_element(by=By.CSS_SELECTOR, value="h1").text == "Presentation of Data quality checker tool for zooscan projects"
-   
-    # Go from doc to app
-    dash_duo.wait_for_page(dash_duo.server_url + '/QC/zooscan/doc', timeout=5)
-    e_go_qc = dash_duo.driver.find_element(by=By.CSS_SELECTOR, value=".go-qc")
-    e_go_qc.click()
-    assert dash_duo.driver.find_element(by=By.CSS_SELECTOR, value="h1").text == "Data quality checks"
-    dash_duo.wait_for_page(dash_duo.server_url + '/QC/zooscan/doc', timeout=5)
