c281bec676125447f523d27f78dd601d86b98ec0
juliecoust <julie.coustenoble@imev-mer.fr>
2022-08-08 16:25:24 +0200

    ODT file names update

--- dash_test/test_unit_zooscan_acquisition.py
+++ dash_test/test_unit_zooscan_acquisition.py
@@ -13,7 +13,7 @@ def test_subBlock_acquisition_check_sieve_bug(dash_duo) :
     #     - the maximum size, column CT acq_max_mesh of the tables ecotaxa_scanID.tsv of the sub-directories of the directories _work
 
     # In the 'sieve BUG' column :
-    project_1="Zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_1" 
+    project_1="zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_1" 
     data_1 = localData.getdata(Mode.TSV, project_1)
     res_1 = impl.check_sieve_bug("check_sieve_bug", Mode.TSV, data_1)
     # "#MISSING ecotaxa table" : If no ecotaxa_scanID.tsv table
@@ -22,7 +22,7 @@ def test_subBlock_acquisition_check_sieve_bug(dash_duo) :
     assert res_1.loc[res_1["List scan ID"]=="wp_d1_1"]["acq max mesh"].values[0] == "#MISSING ecotaxa table"
     assert res_1.loc[res_1["List scan ID"]=="wp_d1_1"]["Sieve Bug"].values[0] == "#MISSING ecotaxa table"
     
-    project_2="Zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_2" 
+    project_2="zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_2" 
     data_2 = localData.getdata(Mode.TSV, project_2)
     res_2 = impl.check_sieve_bug("check_sieve_bug", Mode.TSV, data_2)
     # "#NOT NUMERIC" : If the acq_min and acq_max values are not numerical
@@ -31,7 +31,7 @@ def test_subBlock_acquisition_check_sieve_bug(dash_duo) :
     assert res_2.loc[res_2["List scan ID"]=="wp_d1_1"]["acq max mesh"].values[0] == 1000
     assert res_2.loc[res_2["List scan ID"]=="wp_d1_1"]["Sieve Bug"].values[0] == "#NOT NUMERIC"
     
-    project_3="Zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_3" 
+    project_3="zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_3" 
     data_3 = localData.getdata(Mode.TSV, project_3)
     res_3 = impl.check_sieve_bug("check_sieve_bug", Mode.TSV, data_3)
     # "#SIEVE different from others" : If the acq_min of 1 or more scans differs from the other scans with the same frac ID or the acq_max of 1 or more scans diverges from the other scans with the same frac ID
@@ -44,7 +44,7 @@ def test_subBlock_acquisition_check_sieve_bug(dash_duo) :
     assert res_3.loc[res_3["List scan ID"]=="wp_2_d1_1"]["Sieve Bug"].values[0] == "#SIEVE different from others (d1)"
 
 
-    project_4="Zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_4" 
+    project_4="zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_4" 
     data_4 = localData.getdata(Mode.TSV, project_4)
     res_4 = impl.check_sieve_bug("check_sieve_bug", Mode.TSV, data_4)
     # "#ACQ MIN > ACQ MAX" : If the acq_min is greater than the acq_max for the same FracID (within the same scanID)
@@ -53,7 +53,7 @@ def test_subBlock_acquisition_check_sieve_bug(dash_duo) :
     assert res_4.loc[res_4["List scan ID"]=="wp_d1_1"]["acq max mesh"].values[0] == 100
     assert res_4.loc[res_4["List scan ID"]=="wp_d1_1"]["Sieve Bug"].values[0] == "#ACQ MIN > ACQ MAX"
     
-    project_5="Zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_5" 
+    project_5="zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_5" 
     data_5 = localData.getdata(Mode.TSV, project_5)
     res_5 = impl.check_sieve_bug("check_sieve_bug", Mode.TSV, data_5)
     # "#ACQ MIN = ACQ MAX" : If the acq_min is equal to the acq_max for the same FracID (within the same scanID)
@@ -62,7 +62,7 @@ def test_subBlock_acquisition_check_sieve_bug(dash_duo) :
     assert res_5.loc[res_5["List scan ID"]=="wp_d1_1"]["acq max mesh"].values[0] == 100
     assert res_5.loc[res_5["List scan ID"]=="wp_d1_1"]["Sieve Bug"].values[0] == "#ACQ MIN = ACQ MAX"
 
-    project_6="Zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_6" 
+    project_6="zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_6" 
     data_6 = localData.getdata(Mode.TSV, project_6)
     res_6 = impl.check_sieve_bug("check_sieve_bug", Mode.TSV, data_6)
     # "sieve OK" : If everything is OK
@@ -83,7 +83,7 @@ def test_subBlock_acquisition_check_sieve_bug(dash_duo) :
     assert res_6.loc[res_6["List scan ID"]=="wp_2_d2_1"]["acq max mesh"].values[0] == 1000
     assert res_6.loc[res_6["List scan ID"]=="wp_2_d2_1"]["Sieve Bug"].values[0] == "sieve OK"
 
-    project_7="Zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_7" 
+    project_7="zooscan_test/Zooscan_test_subBlock_acquisition_check_sieve_bug_7" 
     data_7 = localData.getdata(Mode.TSV, project_7)
     res_7 = impl.check_sieve_bug("check_sieve_bug", Mode.TSV, data_7)
     # "#ACQ MIN (dN) ≠ ACQ MAX (dN+1)" : if for the same sampleID whose FracID = d1 or d2 or d3 (comparison between several scanIDs of the same sampleID) the acq_min (dN) ≠ acq_max (d+1)
@@ -101,7 +101,7 @@ def test_subBlock_acquisition_check_motoda_check(dash_duo) :
     # In the column 'MOTODA Fraction' of the report table, is reported :
     # the fraction acq_sub_part of the tables ecotaxa_scanID.tsv of the subdirectories of the _work directories.
     
-    project_1="Zooscan_test/Zooscan_test_subBlock_acquisition_check_motoda_check_1" 
+    project_1="zooscan_test/Zooscan_test_subBlock_acquisition_check_motoda_check_1" 
     data_1 = localData.getdata(Mode.TSV, project_1)
     res_1 = impl.check_motoda_check("check_motoda_check", Mode.TSV, data_1) 
     # In the column 'MOTODA check' of the report table :
@@ -110,7 +110,7 @@ def test_subBlock_acquisition_check_motoda_check(dash_duo) :
     assert res_1.loc[res_1["List scan ID"]=="wp_d1_1"]["MOTODA Fraction"].values[0] == "#MISSING ecotaxa table"
     assert res_1.loc[res_1["List scan ID"]=="wp_d1_1"]["MOTODA check"].values[0] == "#MISSING ecotaxa table"
 
-    project_2="Zooscan_test/Zooscan_test_subBlock_acquisition_check_motoda_check_2" 
+    project_2="zooscan_test/Zooscan_test_subBlock_acquisition_check_motoda_check_2" 
     data_2 = localData.getdata(Mode.TSV, project_2)
     res_2 = impl.check_motoda_check("check_motoda_check", Mode.TSV, data_2) 
     # "#NOT NUMERIC": if the acq_sub_part value is not numeric
@@ -118,7 +118,7 @@ def test_subBlock_acquisition_check_motoda_check(dash_duo) :
     assert res_2.loc[res_2["List scan ID"]=="wp_d1_1"]["MOTODA Fraction"].values[0] == "test"
     assert res_2.loc[res_2["List scan ID"]=="wp_d1_1"]["MOTODA check"].values[0] == "#NOT NUMERIC"
 
-    project_3="Zooscan_test/Zooscan_test_subBlock_acquisition_check_motoda_check_3" 
+    project_3="zooscan_test/Zooscan_test_subBlock_acquisition_check_motoda_check_3" 
     data_3 = localData.getdata(Mode.TSV, project_3)
     res_3 = impl.check_motoda_check("check_motoda_check", Mode.TSV, data_3) 
     # "#Motoda identical": if acq_sub_part is identical throughout the project
@@ -129,7 +129,7 @@ def test_subBlock_acquisition_check_motoda_check(dash_duo) :
     assert res_3.loc[res_3["List scan ID"]=="wp_d2_1"]["MOTODA Fraction"].values[0] == "4"
     assert res_3.loc[res_3["List scan ID"]=="wp_d2_1"]["MOTODA check"].values[0] == "#Identical Motoda"
 
-    project_4="Zooscan_test/Zooscan_test_subBlock_acquisition_check_motoda_check_4" 
+    project_4="zooscan_test/Zooscan_test_subBlock_acquisition_check_motoda_check_4" 
     data_4 = localData.getdata(Mode.TSV, project_4)
     res_4 = impl.check_motoda_check("check_motoda_check", Mode.TSV, data_4) 
     # "#Motoda Fraction ≠ 1 or ≠ ^2": if does not respect → acq_sub_part = 1 or a power of 2
@@ -156,7 +156,7 @@ def test_subBlock_acquisition_check_motoda_check(dash_duo) :
     assert res_4.loc[res_4["List scan ID"]=="bongo_plankton_1"]["MOTODA Fraction"].values[0] == 1
     assert res_4.loc[res_4["List scan ID"]=="bongo_plankton_1"]["MOTODA check"].values[0] == "#Motoda Fraction ≠ ^2"
 
-    project_5="Zooscan_test/Zooscan_test_subBlock_acquisition_check_motoda_check_5" 
+    project_5="zooscan_test/Zooscan_test_subBlock_acquisition_check_motoda_check_5" 
     data_5 = localData.getdata(Mode.TSV, project_5)
     res_5 = impl.check_motoda_check("check_motoda_check", Mode.TSV, data_5) 
     # "Motoda OK" : if everything is OK
@@ -191,7 +191,7 @@ def test_subBlock_acquisition_check_motoda_check(dash_duo) :
 
 #TODO JCE
 # def test_subBlock_acquisition_check_motoda_comparaison(dash_duo) : 
-#     project="Zooscan_test/Zooscan_test_subBlock_process_data_2" 
+#     project="zooscan_test/Zooscan_test_subBlock_process_data_2" 
 #     data = localData.getdata(Mode.TSV, project)
 #     res = impl.check_motoda_comparaison("check_motoda_comparaison", Mode.TSV, data) 
 
@@ -208,7 +208,7 @@ def test_subBlock_acquisition_check_motoda_check(dash_duo) :
 
 #TODO JCE
 # def test_subBlock_acquisition_check_motoda_quality(dash_duo) : 
-#     project="Zooscan_test/Zooscan_test_subBlock_process_data_2" 
+#     project="zooscan_test/Zooscan_test_subBlock_process_data_2" 
 #     data = localData.getdata(Mode.TSV, project)
 #     res = impl.check_motoda_quality("check_motoda_quality", Mode.TSV, data) 
 
@@ -243,7 +243,7 @@ def test_subBlock_acquisition_check_motoda_check(dash_duo) :
 
 #TODO JCE
 # def test_subBlock_acquisition_check_spelling(dash_duo) : 
-#     project="Zooscan_test/Zooscan_test_subBlock_process_data_1" 
+#     project="zooscan_test/Zooscan_test_subBlock_process_data_1" 
 #     data = localData.getdata(Mode.TSV, project)
 #     res = impl.check_spelling("check_spelling", Mode.TSV, data) 
 
