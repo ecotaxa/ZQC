a5e195be999d14d70c4417599d97806e8c3c0c31
juliecoust <julie.coustenoble@imev-mer.fr>
2024-01-10 14:35:25 +0100

    DEV update app name

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import dash_bootstrap_components as dbc
 
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.title = "Quality checks"
+app.title = "Zooscan Quality Checks"
 app._favicon = ("favicon.png")
 server = app.server
